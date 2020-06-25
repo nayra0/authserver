666e8719aa9414c0fa7558dd0ad238ea2d2cabbe
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-06-24 21:53:08 -0300

    Corrigido tamanho do campo senha na tabela usuario

--- database.sql
+++ database.sql
@@ -10,7 +10,7 @@ create table usuario(
    id int auto_increment primary key,
    nome varchar (100),
    email varchar (100),
-   senha varchar (50)
+   senha varchar (256)
 );
 
 -- cria as tabelas
